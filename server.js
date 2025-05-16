adead8082f7c750dd80ba9a25ebb244baceb8114
HarinishreeS <successharini@gmail.com>
2025-05-16 20:58:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api/medicines', medicineRoutes);
 app.use('/api/ml', mlRoutes);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || "port_number";
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 
